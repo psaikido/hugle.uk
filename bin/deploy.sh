6eac684b54d6f96de63a535fb0c853008a35b163
Hugh Carroll <hughie@carrollonline.co.uk>
2020-04-18 17:08:07 +0100

    Include git push to deployment script.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 bundle exec jekyll b
+git push origin master
 
 config="carrollonline_uk@gp-uk.net:~/httpdocs/"
 rsync -av --del /home/hughie/sites/carrollonline.uk/carrollonline/_site/ $config
