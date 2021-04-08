361d0e70bdb6a948c82114119a88de80d5a6b716
Hugh Carroll <hughie@carrollonline.co.uk>
2021-04-08 14:22:25 +0100

    Level 8, first 6 sequences, 2 to go.

--- pages/kites/patterns/l8.js
+++ pages/kites/patterns/l8.js
@@ -85,4 +85,94 @@
       .to(k, {
         rotation: 90 
       })
+      .to(k, {
+        duration: 3,
+        motionPath: { 
+          path: [
+            {x: rtIsh - 40 , y: lowIsh},
+            {x: 0, y: low},
+            {x: lfIsh + 40 , y: lowIsh},
+            {x: lfIsh, y: yMid},
+          ],
+          start: 0,
+          end: 1,
+          curviness: .4,
+          align: 'self',
+        }
+      }, 'slideTurn')
+      .to(kImg, {
+        rotation: '-180_cw',
+        duration: 3,
+        delay: 0,
+      }, "<slideTurn")
+      .to(k, { y: hiIsh })
+      .to(k, { y: yMid })
+      .to(k, { x: lfIsh - 80 })
+      .to(k, { x: lfIsh })
+      .to(k, { rotation: '-=45_ccw' })
+      .to(k, { rotation: '+=90_cw' })
+      .to(k, { rotation: '-=45_ccw' })
+      .to(kImg, {
+        rotation: 0,
+      })
+      .to(k, {
+        x: rtIsh,
+        y: hiIsh,
+        rotation: '-=45',
+      })
+      .to(k, {
+        x: rtIsh - 70,
+        y: hi,
+      })
+      .to(k, {
+        x: rtIsh,
+        y: hiIsh,
+      })
+      .to(k, {
+        x: rtIsh + 70,
+        y: hiIsh - 70,
+      })
+      .to(k, {
+        x: rtIsh,
+        y: hiIsh,
+      })
+      .to(k, {
+        rotation: '-=45'
+      })
+      .to(k, {
+        rotation: '+=90'
+      })
+      .to(k, {
+        rotation: '-=45'
+      })
+      .to(k, {
+        x: lfIsh,
+        y: lowIsh,
+        rotation: '-=180'
+      })
+      .to(k, {
+        x: lfIsh - 70,
+        y: lowIsh - 70,
+      })
+      .to(k, {
+        x: lfIsh,
+        y: lowIsh,
+      })
+      .to(k, {
+        x: lfIsh - 70,
+        y: lowIsh + 70,
+      })
+      .to(k, {
+        x: lfIsh,
+        y: lowIsh,
+      })
+      .to(k, {
+        rotation: '+=45'
+      })
+      .to(k, {
+        rotation: '-=90'
+      })
+      .to(k, {
+        rotation: '+=45'
+      })
   }
