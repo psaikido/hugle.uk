0d1b98fdb9d57b264c37038126b4b924ceccad60
Hugh Carroll <hughie@carrollonline.co.uk>
2021-04-09 18:04:09 +0100

    Improve L6 a bit.

--- pages/kites/patterns/l6.js
+++ pages/kites/patterns/l6.js
@@ -27,6 +27,7 @@ function L6() {
       rotation: '-=540_ccw',
       duration: 1,
     })
+    // Bottom turns right then left.
     .to(k, {
       x: rt,
       duration: 1.5,
@@ -54,6 +55,7 @@ function L6() {
       rotation: '180_cw',
       duration: .5,
     })
+    // Reverse up, spin, go right.
     .to(k, {
       y: yMid,
       duration: 2,
@@ -66,6 +68,7 @@ function L6() {
       x: rtIsh,
       duration: 1,
     })
+    // Clock.
     .to(k, {
       rotation: '+=45_cw',
       duration: .3,
@@ -86,19 +89,22 @@ function L6() {
       x: lfIsh,
       duration: 1,
     })
+    // Iron Cross.
     .to(k, {
-      delay: 0,
+      delay: 1,
       duration: 2,
       ease: 'sine.inOut', 
       motionPath: { 
         path: [
+          {x: lfIsh - 80, y: yMid},
           {x: lfIsh, y: yMid},
-          {x: -30, y: yMid - 30},
+          {x: -40, y: yMid - 30},
           {x: 0, y: hiIsh},
+          {x: 0, y: hi},
         ],
-        start: 0,
-        end: 1,
-        curviness: 1,
+        start: .42,
+        end: .95,
+        curviness: .5,
         type: 'soft',
         autoRotate: -90,
       }
@@ -111,11 +117,11 @@ function L6() {
         path: [
           {x: 30, y: yMid - 30},
           {x: rtIsh, y: yMid},
+          {x: rt, y: yMid},
         ],
         start: 0,
-        end: 1,
-        curviness: 1,
-        type: 'soft',
+        end: .7,
+        curviness: .5,
         autoRotate: 90,
       }
     })
@@ -125,13 +131,15 @@ function L6() {
       ease: 'sine.inOut', 
       motionPath: { 
         path: [
-          {x: 30, y: yMid + 30},
+          {x: rtIsh + 80, y: yMid},
+          {x: rtIsh, y: yMid},
+          {x: 40, y: yMid + 30},
           {x: 0, y: lowIsh},
+          {x: 0, y: 0},
         ],
-        start: 0,
-        end: 1,
-        curviness: 1,
-        type: 'soft',
+        start: .09,
+        end: .9,
+        curviness: .5,
         autoRotate: -90,
       }
     })
@@ -141,15 +149,21 @@ function L6() {
       ease: 'sine.inOut', 
       motionPath: { 
         path: [
-          {x: -30, y: yMid + 30},
+          {x: 0, y: ground},
+          {x: 0, y: lowIsh},
+          {x: - 40, y: lowIsh - 60},
           {x: lfIsh, y: yMid},
+          {x: lfIsh - 80, y: yMid},
         ],
-        start: 0,
-        end: 1,
-        curviness: 1,
-        type: 'soft',
+        start: .20,
+        end: .9,
+        curviness: .5,
         autoRotate: 90,
       }
+    })
+    .to(k, {
+      rotation: 0,
+      duration: .5,
     })
     .to(k, {
       delay: 0,
@@ -163,8 +177,7 @@ function L6() {
         ],
         start: 0,
         end: 1,
-        curviness: 1,
-        type: 'soft',
+        curviness: .5,
         autoRotate: 90,
       }
     })
