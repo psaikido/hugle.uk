47dc1a00d4ed3ef14b30c853140fdd96120e22ef
Hugh Carroll <hughie@carrollonline.co.uk>
2021-04-10 09:49:12 +0100

    M11 added.

---
+++ pages/kites/patterns/m11.js
@@ -0,0 +1,96 @@
+function M11() {
+  reset();
+
+  timeline
+    .to(k, {y: lowIsh})
+    .to(k, {rotation: '+=540_cw'})
+    .to(k, {y: hiIsh})
+    .to(k, {rotation: '-=450_ccw'})
+    .to(k, {y: lowIsh})
+    .to(k, {rotation: '-=360_ccw'})
+    .to(k, {x: rtIsh})
+    .to(k, {
+      rotation: '+=360_cw',
+    })
+    .to(k, {
+      x: 0,
+      delay: 0,
+    })
+    .to(k, {
+      x: rtIsh,
+      y: lowIsh + 20,
+      rotation: '-=270_ccw',
+    })
+    .to(k, {rotation: '+=90_cw'})
+    .to(k, {rotation: '+=90_cw'})
+    .to(k, {rotation: '+=90_cw'})
+    .to(k, {rotation: '+=90_cw'})
+    .to(k, {rotation: '+=90_cw'})
+    .to(k, {x: lfIsh,})
+    .to(k, {rotation: '-=270_ccw'})
+    .to(k, {y: hiIsh,})
+    .to(k, {
+      x: 0,
+      y: hi,
+      rotation: '+=180_cs'
+    })
+    .to(k, {y: lowIsh,})
+    .to(k, {rotation: '-=90_ccw'})
+    .to(k, {rotation: '-=90_ccw'})
+    .to(k, {rotation: '-=90_ccw'})
+    .to(k, {rotation: '-=90_ccw'})
+    .to(k, {rotation: '+=360_cw'})
+    // Slide circle.
+    .to(k, {
+      delay: 0,
+      duration: 6,
+      ease: 'linear', 
+      motionPath: { 
+        path: [
+          {x: rtIsh, y: yMid},
+          {x: 0, y: hiIsh},
+          {x: lfIsh, y: yMid},
+          {x: 0, y: lowIsh},
+        ],
+        start: 0,
+        end: 1,
+        curviness: 1,
+      }
+    }, 'slide-circle-m11')
+    .to(kImg, {
+      rotation: '-=360_ccw',
+      duration: 6,
+      delay: 0,
+    }, "<")
+    .to(k, {
+      duration: 3,
+      ease: 'linear', 
+      motionPath: { 
+        path: [
+          {x: 0, y: lowIsh},
+          {x: lfIsh, y: yMid},
+          {x: 0, y: hiIsh},
+        ],
+        start: 0,
+        end: 1,
+        curviness: 1,
+      }
+    })
+    .to(kImg, {
+      rotation: '+=180_cw',
+      duration: 3,
+    }, "<")
+    .to(k, {y: hi})
+    .to(kImg, {
+      rotation: '-=180_ccw',
+    })
+    .to(k, {y: yMid})
+    .to(kImg, {
+      rotation: '-=360_ccw',
+    })
+    .to(k, {y: low})
+    .to(kImg, {
+      rotation: '+=630_cw',
+    })
+    .to(k, {y: ground})
+}
