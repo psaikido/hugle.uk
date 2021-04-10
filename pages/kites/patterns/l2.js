340eba53910bd07ce6d98d9adf7b55e93a8f55bb
Hugh Carroll <hughie@carrollonline.co.uk>
2021-04-10 11:54:53 +0100

    Improve L2.

--- pages/kites/patterns/l2.js
+++ pages/kites/patterns/l2.js
@@ -9,18 +9,17 @@ function L2() {
     .to(k, {x: rt, duration: 3})
     .to(k, {rotation: '-90_ccw'})
     .to(k, {x: 0, duration: 1.5})
-    .to(k, {rotation: '270_cw'})
-    .to(k, {x: lf, duration: 2})
+    .to(k, {rotation: '+=360_cw', delay: 0,})
+    .to(k, {x: lf, duration: 2, delay: 0,})
     .to(k, {rotation: '90_cw'})
     .to(k, {x: 0, duration: 1.5})
-    .to(k, {rotation: '450_ccw'})
-    .to(k, {rotation: '360_shortest', delay: 0})
+    .to(k, {rotation: '-=450_ccw', delay: 0,})
     .to(k, {y: hi})
-    .to(k, {rotation: '180_cw'})
+    .to(k, {rotation: '+=180_cw'})
     .to(k, {
       y: low,
       duration: .5,
-      ease: 'sine.in'
+      ease: 'power3.in'
     })
     .to(k, {rotation: '0_cw'})
     .to(k, {y: 0})
