70d577781a5c81bd2e3e94feb076d61337adbbb0
Hugh Carroll <hughie@carrollonline.co.uk>
2021-06-06 21:03:05 +0100

    We only need to push on github pages so no deploy script needed.

--- bin/deploy.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-JEKYLL_ENV=production bundle exec jekyll b
-git push origin master
-
-config="carrollonline_uk@gp-uk.net:~/httpdocs/"
-rsync -av --del /home/hughie/sites/carrollonline.uk/carrollonline/_site/ $config
