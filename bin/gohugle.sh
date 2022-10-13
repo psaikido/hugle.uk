baab0360d25a5e8cbdf482c276483cea89dfc300
Hugh Carroll <hughie@carrollonline.co.uk>
2022-10-13 13:59:31 +0100

    Rejig the layout with subgrid.

--- bin/gohugle.sh
+++ bin/gohugle.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 hugo
+
+# For working in dev we output the pagefind index to the local static directory.
 npm_config_yes=true npx pagefind --source "public" --bundle-dir ../static/_pagefind
 hugo server
+
+# For live the pagefind index default to output to the public directory.
+# npx pagefind --source "public" 
 
