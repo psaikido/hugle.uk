1a580e43a20ae4374fec1ae7eafae1f2daf04aac
Hugh Carroll <hughie@carrollonline.co.uk>
2023-10-12 03:35:23 +0100

    Update the local pagefind build.

--- bin/gohugle.sh
+++ bin/gohugle.sh
@@ -3,8 +3,8 @@ rm -rf ./public
 hugo
 
 # For working in dev we output the pagefind index to the local static directory.
-npm_config_yes=true npx pagefind --source "public" --bundle-dir ../static/_pagefind
-hugo server
+npm_config_yes=true npx pagefind@latest --site "public" --output-subdir "../static/_pagefind"
+hugo serve
 
 # For live the pagefind index default to output to the public directory.
 # npx pagefind --source "public" 
